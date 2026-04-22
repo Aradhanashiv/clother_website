f12decb875812ef32ea2ef6fcaa34113564151bd
aradhana <aradhanashivhare374@gmail.com>
2026-04-22 19:41:37 +0530

    ui

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 import "dotenv/config"
 import express from 'express'
-import {connectMongoDB} from '../config/db.js'
+import {connectMongoDB} from './config/db.js'
 import cors from 'cors'
 import cookieParser from "cookie-parser"
-import userRoute from '../routes/userRoute.js'
-import homeRoute from '../routes/homeRoute.js'
-import productRoute from '../routes/productRoute.js'
-import categoryRoute from '../routes/categoryRoute.js'
-import searchRoute from '../routes/searchRoute.js'
+import userRoute from './routes/userRoute.js'
+import homeRoute from './routes/homeRoute.js'
+import productRoute from './routes/productRoute.js'
+import categoryRoute from './routes/categoryRoute.js'
+import searchRoute from './routes/searchRoute.js'
 
 
 const app = express()
