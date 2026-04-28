7057c456ec8808ed37b7d4c2e8a5c6e9b5b31e37
aradhana <aradhanashivhare374@gmail.com>
2026-04-28 23:20:00 +0530

    ui

--- frontend/src/components/SubHeader.jsx
+++ frontend/src/components/SubHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SubHeader = () => {
   return (
-    <section className="bg-pink-50 w-full min-h-[25vh] relative mt-[70px]">
+    <section className="bg-pink-50 w-full min-h-[25vh] relative md:mt-[70px] mt-[100px]">
   <div className="absolute inset-0 flex flex-col justify-center items-center">
     <p className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-cyan-500 to-[#fc00ff] animate-pulse">
       Click. Buy. Smile.
