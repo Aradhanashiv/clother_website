6891f9ab2c9953dea641375f9ceb159da488f589
aradhana <aradhanashivhare374@gmail.com>
2026-04-28 23:20:57 +0530

    ui

--- frontend/src/components/SubHeader.jsx
+++ frontend/src/components/SubHeader.jsx
@@ -4,10 +4,10 @@ const SubHeader = () => {
   return (
     <section className="bg-pink-50 w-full min-h-[25vh] relative md:mt-[70px] mt-[100px]">
   <div className="absolute inset-0 flex flex-col justify-center items-center">
-    <p className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-cyan-500 to-[#fc00ff] animate-pulse">
+    <p className="md:text-4xl text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-cyan-500 to-[#fc00ff] animate-pulse">
       Click. Buy. Smile.
     </p>
-    <p className="mt-4 text-5xl text-transparent bg-clip-text bg-gradient-to-r from-[#fc00ff] to-blue-500 animate-bounce">
+    <p className="mt-4 md:text-5xl text-4xl text-transparent bg-clip-text bg-gradient-to-r from-[#fc00ff] to-blue-500 animate-bounce">
       Shopping Made Simple, Fast & Fun
     </p>
   </div>
