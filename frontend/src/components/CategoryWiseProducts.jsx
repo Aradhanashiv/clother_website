bd5eb259fbad62b003dee154534d231ba0e0a7bc
aradhana <aradhanashivhare374@gmail.com>
2026-04-29 16:02:06 +0530

    ui

--- frontend/src/components/CategoryWiseProducts.jsx
+++ frontend/src/components/CategoryWiseProducts.jsx
@@ -34,7 +34,7 @@ const CategoryWiseProducts = ({}) => {
       <button className="px-6 py-4" onClick={() => navigate("/")}>
             <FaBackward size={25} />
           </button> 
-        <h1 className='font-bold text-5xl text-pink-800 text-center mb-[20px] p-[30px]'>Your Fashion, Your Style</h1>
+        <h1 className='font-bold text-5xl text-pink-800 text-center mb-[20px] md:p-[30px] p-[15px]'>Your Fashion, Your Style</h1>
         <div className="grid md:grid-cols-4 grid-cols-2 gap-5 mx-8">
          {myProductsData.map((product,index) => 
             <div key={index} className="overflow-hidden rounded-xl">
