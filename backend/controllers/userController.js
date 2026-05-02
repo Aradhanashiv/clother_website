c4f95b5ea9de6cb2f95cab98c09a32abd6e2c193
aradhana <aradhanashivhare374@gmail.com>
2026-05-02 14:16:49 +0530

    ui

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -74,7 +74,7 @@ export const handleSignOut = (req,res) => {
   try {
     res.clearCookie("token" , {
       httpOnly : true,
-      sameSite: 'strict',
+      sameSite: 'None',
       secure:true
     })
     return res.status(200).json({success: true, message:"User Deleted Successfully"})
