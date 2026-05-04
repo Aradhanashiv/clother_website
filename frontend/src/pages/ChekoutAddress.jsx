22dc42d9023fc049a8260575b94f047337a0713d
aradhana <aradhanashivhare374@gmail.com>
2026-05-04 17:33:30 +0530

    ui

--- frontend/src/pages/ChekoutAddress.jsx
+++ frontend/src/pages/ChekoutAddress.jsx
@@ -85,10 +85,10 @@ const ChekoutAddress = () => {
           <div className="flex flex-col items-center m-auto md:w-[80%] w-full flex"> 
          
           <div className="w-full rounded-lg px-2 py-3 m-auto">
-              {!addresses ? 
+              {!userData.addresses ? 
               ( <p className="py-3 text-lg text-gray-800">No Address Saved</p> )
               :
-               (addresses.map((address, i) => (
+               (userData.addresses.map((address, i) => (
               <div key={i} className="flex border border-gray-300 rounded-lg mt-1  m-auto px-3 py-4 bg-white/70">
                 <div className="w-full px-4">
                   <p className="font-bold">{address.fullName}</p>
